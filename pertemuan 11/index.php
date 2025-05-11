7dd4542a5e8771e534e91b0da0673e0796b485ee
luckyboy002 <151297077+luckyboy002@users.noreply.github.com>
2025-05-11 18:39:51 +0700

    Add files via upload

---
+++ pertemuan 11/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+</head>
+<body>
+    <form action="tampil.php" method="post">
+        <label for="nama"> masukan nama </label>
+        <input type="text" name="nama">
+        <label for="usia"> masukan usia </label>
+        <input type="text" name="usia">
+        <label for="alamat"> masukan alamat </label>
+        <input type="text" name="alamat">
+        <label for="npm"> masukan npm</label>
+        <input type="text" name="npm">
+        <input type="submit">
+    </form>
+</body>
+</html>
