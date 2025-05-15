72624f50626ee5fb2f6b262cd93b3511338fc3a9
luckyboy002 <151297077+luckyboy002@users.noreply.github.com>
2025-05-15 15:04:41 +0700

    Add files via upload

---
+++ pertemuan 12/tampil.php
@@ -0,0 +1,14 @@
+<?php
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $npm = $_POST['npm'];
+    $usia = $_POST['usia'];
+
+    echo "Nama anda adalah: ".$nama;
+    echo "<br>";
+    echo "Usia anda adalah: ".$alamat;
+    echo "<br>";
+    echo "NPM anda adalah: ".$npm;
+    echo "<br>";
+    echo "Usia anda adalah: ".$usia;
+?>
